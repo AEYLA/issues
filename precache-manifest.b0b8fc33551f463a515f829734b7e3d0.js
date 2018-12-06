b088f51a37be640c0cfde84c49f4268063339dc1
Arthur Stokes <arthur.stokes@sabre.com>
2018-12-06 23:23:50 +0000

    Updates

---
+++ precache-manifest.b0b8fc33551f463a515f829734b7e3d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee08f6c2e0cb0e4ed2",
+    "url": "/issues/static/js/runtime~main.5bee08f6.js"
+  },
+  {
+    "revision": "2fb84b0c428d04289bc2",
+    "url": "/issues/static/js/main.2fb84b0c.chunk.js"
+  },
+  {
+    "revision": "79a88edf3234fbfcf04f",
+    "url": "/issues/static/js/1.79a88edf.chunk.js"
+  },
+  {
+    "revision": "2fb84b0c428d04289bc2",
+    "url": "/issues/static/css/main.8b7f567d.chunk.css"
+  },
+  {
+    "revision": "73afdd9c5f7ba153543b5086ec06148b",
+    "url": "/issues/index.html"
+  }
+];
